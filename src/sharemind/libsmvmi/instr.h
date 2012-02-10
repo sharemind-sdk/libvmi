4fcec01e1ad09e5c53b66cac11faa562f28e2707
Jaak Ristioja <jaak.ristioja@cyber.ee>
2012-02-10 11:00:04 +0200

    Upgraded to updated submodules.

--- src/sharemind/libsmvmi/instr.h
+++ src/sharemind/libsmvmi/instr.h
@@ -19,7 +19,7 @@
 extern "C" {
 #endif
 
-#define SMVMI_MAKE_INSTR(a,b,c,d,e,f,g,h) SM_MAKE_UINT64_BE(a,b,c,d,e,f,g,h)
+#define SMVMI_MAKE_INSTR(a,b,c,d,e,f,g,h) SHAREMIND_MAKE_UINT64_BE(a,b,c,d,e,f,g,h)
 
 typedef struct {
     const char * fullName;
